a46efefc08b15d0c32886d15a47a8ad092c05c66
Kimberly Lu <kimlu4@comcast.net>
2017-07-21 14:53:02 -0700

    add delivery option to pizza order.

--- js/scripts.js
+++ js/scripts.js
@@ -7,28 +7,60 @@ function Pizza(topping, size, quantity) {
 
 var initialPrice = 10;
 
- Pizza.prototype.calcPrice = function() {
-   return (initialPrice + this.topping + this.size) * this.quantity;
+Pizza.prototype.calcPrice = function() {
+  return (initialPrice + this.topping + this.size) * this.quantity;
  }
 
- function resetFields() {
+function Customer(name, street, city, state, zip) {
+  this.name = name;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+  }
+
+Customer.prototype.fullInfo = function() {
+  return this.name + "<br>" + this.street + "<br>" + this.city + " , " + this.state + " " + this.zip;
+}
+
+function resetFields() {
   $("#top").val("");
   $("#size").val("");
   $("#qty").val("");
+  $("#name").val("");
+  $("#street").val("");
+  $("#city").val("");
+  $("#state").val("");
+  $("#zip").val("");
   $(".form-control").val("");
 }
 
 //UI logic
 $(document).ready(function() {
+  $("#deliveryOption").click(function() {
+    $("#delivery").show();
+  });
+
   $("#pizza-form").submit(function(event) {
     event.preventDefault();
     var inputTopping = parseInt($("#top").val());
     var inputSize = parseInt($("#size").val());
     var inputQty = parseInt($("#qty").val());
+    var inputName = $("#name").val();
+    var inputStreet = $("#street").val();
+    var inputCity = $("#city").val();
+    var inputState= $("#state").val();
+    var inputZip = $("#zip").val();
     var newPizzaOrder = new Pizza(inputTopping, inputSize, inputQty);
-    
-  $("#total").text("$" + newPizzaOrder.calcPrice());
-  $("#receipt").show();
-});
-resetFields();
+    var newCustomer = new Customer(inputName, inputStreet, inputCity, inputState, inputZip);
+    $("#total").text("$" + newPizzaOrder.calcPrice());
+    $("#customer").append(newCustomer.fullInfo() + "<span class='btn btn-primary' id='done'>Got it!</span>");
+    $("#receipt").show();
+    $("#delivery").hide();
+
+    resetFields();
+  });
+  $("#done").click(function() {
+    $("#customer").parent().remove();
+  });
 });
